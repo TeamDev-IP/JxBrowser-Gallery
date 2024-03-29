5643658d37f7833c3f148daf62d1b2e42f1e9893
Dmitry Kuzmin <dmitry.kuzmin@teamdev.com>
2024-03-29 03:57:33 +0100

    Reorder methods

--- chart-rendering/client/src/main/index.js
+++ chart-rendering/client/src/main/index.js
@@ -20,40 +20,6 @@
 
 import Chart from 'chart.js/auto';
 
-function csvToArray(strData, strDelimiter) {
-    strDelimiter = (strDelimiter || ',');
-    var objPattern = new RegExp(
-        (
-            '(\\' + strDelimiter + '|\\r?\\n|\\r|^)' +
-            '(?:"([^"]*(?:""[^"]*)*)"|' +
-            '([^"\\' + strDelimiter + '\\r\\n]*))'
-        ),
-        'gi',
-    );
-    var arrData = [[]];
-    var arrMatches = null;
-    while (arrMatches = objPattern.exec(strData)) {
-        var strMatchedDelimiter = arrMatches[1];
-        if (
-            strMatchedDelimiter.length &&
-            strMatchedDelimiter !== strDelimiter
-        ) {
-            arrData.push([]);
-        }
-        var strMatchedValue;
-        if (arrMatches[2]) {
-            strMatchedValue = arrMatches[2].replace(
-                new RegExp('""', 'g'),
-                '"',
-            );
-        } else {
-            strMatchedValue = arrMatches[3];
-        }
-        arrData[arrData.length - 1].push(strMatchedValue);
-    }
-    return (arrData);
-}
-
 window.drawFossilFuelsConsumptionChart = (canvas, csvData) => {
     const parsedData = csvToArray(csvData);
     new Chart(
@@ -86,4 +52,38 @@ window.drawFossilFuelsConsumptionChart = (canvas, csvData) => {
             },
         },
     );
+}
+
+function csvToArray(strData, strDelimiter) {
+    strDelimiter = (strDelimiter || ',');
+    var objPattern = new RegExp(
+        (
+            '(\\' + strDelimiter + '|\\r?\\n|\\r|^)' +
+            '(?:"([^"]*(?:""[^"]*)*)"|' +
+            '([^"\\' + strDelimiter + '\\r\\n]*))'
+        ),
+        'gi',
+    );
+    var arrData = [[]];
+    var arrMatches = null;
+    while (arrMatches = objPattern.exec(strData)) {
+        var strMatchedDelimiter = arrMatches[1];
+        if (
+            strMatchedDelimiter.length &&
+            strMatchedDelimiter !== strDelimiter
+        ) {
+            arrData.push([]);
+        }
+        var strMatchedValue;
+        if (arrMatches[2]) {
+            strMatchedValue = arrMatches[2].replace(
+                new RegExp('""', 'g'),
+                '"',
+            );
+        } else {
+            strMatchedValue = arrMatches[3];
+        }
+        arrData[arrData.length - 1].push(strMatchedValue);
+    }
+    return (arrData);
 }
