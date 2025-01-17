468358afe36028b69373c7dff0a34700ff1807c5
Ivan Diachenko <ivan.diachenko@teamdev.com>
2025-01-17 10:44:23 +0100

    Add FontSizeProvider in the DOM tree

--- desktop-web-app/web-app/shadcn/src/App.tsx
+++ desktop-web-app/web-app/shadcn/src/App.tsx
@@ -15,46 +15,51 @@ import {Appearance} from "@/components/appearance.tsx";
 import {NavigationItem} from "@/components/navigation-item.tsx";
 import {Bell, Settings, SquareUser, SunMoon} from "lucide-react";
 import {Notifications} from "@/components/notifications.tsx";
+import {FontSizeProvider} from "@/components/font-size-provider.tsx";
 
 function App() {
     return (
         <ThemeProvider>
-            <SidebarProvider className="space-y-2 space-x-2 h-full">
-                <Sidebar className="!h-auto" collapsible={"none"} side="left">
-                    <SidebarContent className="p-4">
-                        <SidebarGroup/>
-                        <SidebarGroupContent>
-                            <SidebarMenu>
-                                <NavigationItem title={"Account"} url={"/"} icon={SquareUser}/>
-                            </SidebarMenu>
-                        </SidebarGroupContent>
-                        <SidebarGroupLabel className={"pt-3"}>Preferences</SidebarGroupLabel>
-                        <SidebarGroupContent>
-                            <SidebarMenu>
-                                <NavigationItem title={"General"} url={"/prefs/general"}
-                                                icon={Settings}/>
-                                <NavigationItem title={"Appearance"} url={"/prefs/appearance"}
-                                                icon={SunMoon}/>
-                                <NavigationItem title={"Notifications"} url={"/prefs/notifications"}
-                                                icon={Bell}/>
-                            </SidebarMenu>
-                        </SidebarGroupContent>
-                        <SidebarGroup/>
-                    </SidebarContent>
-                </Sidebar>
-                <main className="w-full p-8">
-                    <div className="w-full">
-                        <Router>
-                            <Routes>
-                                <Route path={"/"} element={<UserAccount/>}/>
-                                <Route path={"/prefs/general"} element={<General/>}/>
-                                <Route path={"/prefs/appearance"} element={<Appearance/>}/>
-                                <Route path={"/prefs/notifications"} element={<Notifications/>}/>
-                            </Routes>
-                        </Router>
-                    </div>
-                </main>
-            </SidebarProvider>
+            <FontSizeProvider>
+                <SidebarProvider className="space-y-2 space-x-2 h-full">
+                    <Sidebar className="!h-auto" collapsible={"none"} side="left">
+                        <SidebarContent className="p-4">
+                            <SidebarGroup/>
+                            <SidebarGroupContent>
+                                <SidebarMenu>
+                                    <NavigationItem title={"Account"} url={"/"} icon={SquareUser}/>
+                                </SidebarMenu>
+                            </SidebarGroupContent>
+                            <SidebarGroupLabel className={"pt-3"}>Preferences</SidebarGroupLabel>
+                            <SidebarGroupContent>
+                                <SidebarMenu>
+                                    <NavigationItem title={"General"} url={"/prefs/general"}
+                                                    icon={Settings}/>
+                                    <NavigationItem title={"Appearance"} url={"/prefs/appearance"}
+                                                    icon={SunMoon}/>
+                                    <NavigationItem title={"Notifications"}
+                                                    url={"/prefs/notifications"}
+                                                    icon={Bell}/>
+                                </SidebarMenu>
+                            </SidebarGroupContent>
+                            <SidebarGroup/>
+                        </SidebarContent>
+                    </Sidebar>
+                    <main className="w-full p-8">
+                        <div className="w-full">
+                            <Router>
+                                <Routes>
+                                    <Route path={"/"} element={<UserAccount/>}/>
+                                    <Route path={"/prefs/general"} element={<General/>}/>
+                                    <Route path={"/prefs/appearance"} element={<Appearance/>}/>
+                                    <Route path={"/prefs/notifications"}
+                                           element={<Notifications/>}/>
+                                </Routes>
+                            </Router>
+                        </div>
+                    </main>
+                </SidebarProvider>
+            </FontSizeProvider>
         </ThemeProvider>
     )
 }
